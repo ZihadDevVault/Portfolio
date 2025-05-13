5966270024e7146715c5f5da6e34a4af28c76bee
ZihadDevVault <engr.zihadmia@gmail.com>
2025-05-13 14:04:22 +0600

    resume download problem solve

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -41,7 +41,7 @@ const Header=({isDarkMode})=>{
                     initial={{y:30 , opacity:0}}
                     whileInView={{y:0, opacity:1}}
                     transition={{duration:0.6, delay:1.2}}
-                    href="#sample-rusume.pdf" download className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white hover:bg-lightHover  dark:text-black">Download CV <img src={assets.download_icon} alt="profile_img"  className="w-4"/></motion.a>
+                    href="/sample-resume.pdf" download className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white hover:bg-lightHover  dark:text-black">Download CV <img src={assets.download_icon} alt="profile_img"  className="w-4"/></motion.a>
                 </div>
 
            
